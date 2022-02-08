8b64f12ab9b9fce0edb18390768997ff2ea3a563
JuanAira <juanaira_01@hotmail.com>
2022-02-07 21:43:38 -0300

    Add bases (section two)

---
+++ bases-01/src/bases/CounterHook.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { useCounter } from '../hooks/useCounter'
+
+export const CounterHook = () => {
+  const { counter, elementToAnimate, handleClick } = useCounter({ maxCount: 15 })
+
+  return (
+        <>
+            <h1>Counter Hook:</h1>
+            <h2 ref={ elementToAnimate }>{ counter }</h2>
+
+            <button onClick={ handleClick }>
+                +1
+            </button>
+        </>
+  )
+}
